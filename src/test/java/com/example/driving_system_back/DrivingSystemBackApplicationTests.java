685c5374b4a5ded96c8133426b34b56ee58f1506
DoubleHong <greathongtian@gmail.com>
2023-06-05 16:46:49 +0800

    Hong

--- src/test/java/com/example/driving_system_back/DrivingSystemBackApplicationTests.java
+++ src/test/java/com/example/driving_system_back/DrivingSystemBackApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.driving_system_back;
 
+import com.example.driving_system_back.mapper.StudentMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DrivingSystemBackApplicationTests {
 
+    @Autowired
+    StudentMapper studentMapper;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void test(){
+        System.out.println(studentMapper.selectList(null));
     }
 
 }
