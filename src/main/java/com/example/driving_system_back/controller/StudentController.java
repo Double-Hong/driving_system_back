196dac5a33bee754fa970c0195dff99226b06a23
hdl <183165855@qq.com>
2023-06-24 10:53:37 +0800

    Changes

--- src/main/java/com/example/driving_system_back/controller/StudentController.java
+++ src/main/java/com/example/driving_system_back/controller/StudentController.java
@@ -1,12 +1,23 @@
 package com.example.driving_system_back.controller;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.driving_system_back.common.vo.Result;
 import com.example.driving_system_back.entity.StudentEntity;
 import com.example.driving_system_back.mapper.StudentMapper;
+import com.example.driving_system_back.service.StudentService;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * <p>
@@ -22,10 +33,73 @@ public class StudentController {
 
     @Autowired
     StudentMapper studentMapper;
+    @Autowired
+    private StudentService studentService;
 
     @PostMapping("/studentLogin")
     public List<StudentEntity> studentLogin(@RequestBody StudentEntity studentEntity){
         return studentMapper.selectList(new QueryWrapper<StudentEntity>().eq("username",studentEntity.getUsername()).eq("`password`",studentEntity.getPassword()));
     }
 
+
+    public List<StudentEntity> findStudentByStudentName(String Name){
+        return studentMapper.selectList(Wrappers.<StudentEntity>lambdaQuery().like(StudentEntity::getStudentName, Name));
+    }
+
+    @GetMapping
+    public Result<?> findAllStudent(){
+        return Result.success(findStudentByStudentName(""));
+    }
+    //查找学生
+    @PostMapping("/getStudentList/{studentName}")
+    public Result<?>getStudentList (@PathVariable String studentName){
+        List<StudentEntity> students=findStudentByStudentName(studentName);
+        return Result.success(students);
+    }
+
+
+    //修改用户
+    @PostMapping
+    public Result<?> updateStudent(@RequestBody StudentEntity s){
+        if(studentService.updateById(s)){
+//            String name="";
+//            List<StudentEntity> students=findStudentByStudentName(name);
+            return Result.success();
+        }
+        else{
+            return Result.fail();
+        }
+
+    }
+
+    //删除用户
+    @DeleteMapping("/{id}")
+    public Result<?> deleteById(@PathVariable Integer id){
+        if(studentService.removeById(id)){
+            String name="";
+            List<StudentEntity> students=findStudentByStudentName(name);
+            return Result.success(students);
+        }
+        else{
+            return Result.fail();
+        }
+
+    }
+
+    //新增用户
+    @PostMapping("/addStudent")
+    public Result<?> insertStudent(@RequestBody StudentEntity student){
+        String s = UUID.randomUUID().toString();
+        student.setStudentId(s);
+        student.setPassword("123456");
+        studentService.save(student);
+        return Result.success();
+    }
+
+    //通过id查找
+    @GetMapping("/findById/{id}")
+    public Result<?> findStudentById(@PathVariable String id){
+        StudentEntity student= studentService.getById(id);
+        return Result.success(student);
+    }
 }
--- src/main/java/com/example/driving_system_back/entity/StudentEntity.java
+++ src/main/java/com/example/driving_system_back/entity/StudentEntity.java
@@ -4,21 +4,25 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.sql.Date;
 import java.time.LocalDate;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author Double-Hong and My-way and 何栋梁 and 肖雅云
  * @since 2023-06-03 14:03:41
  */
+@Data
 @Getter
 @Setter
 @Accessors(chain = true)
@@ -56,8 +60,9 @@ public class StudentEntity {
     @TableField("gender")
     private String gender;
 
+
     @TableField("birthday")
-    private LocalDate birthday;
+    private Date birthday;
 
     @TableField("phone")
     private String phone;
@@ -67,6 +72,5 @@ public class StudentEntity {
 
     @TableField("study_type")
     private String studyType;
-
 
 }
