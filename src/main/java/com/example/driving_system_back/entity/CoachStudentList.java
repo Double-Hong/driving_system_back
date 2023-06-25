34d1e7d67cc4dd526905f5dc14fd96f396ce7023
DoubleHong <greathongtian@gmail.com>
2023-06-25 16:24:19 +0800

    Hong

---
+++ src/main/java/com/example/driving_system_back/entity/CoachStudentList.java
@@ -0,0 +1,49 @@
+package com.example.driving_system_back.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 
+ * @TableName coach_student_list
+ */
+@TableName(value ="coach_student_list")
+@Data
+public class CoachStudentList implements Serializable {
+    /**
+     * 
+     */
+    private String practice_id;
+
+    /**
+     * 
+     */
+    private String student_id;
+
+    /**
+     * 
+     */
+    private String student_name;
+
+    /**
+     * 
+     */
+    private String coach_id;
+
+    /**
+     * 
+     */
+    private Date practice_datetime;
+
+    /**
+     * 申请状态,教练同意还是拒绝
+     */
+    private Integer application_state;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/example/driving_system_back/mapper/CoachStudentListMapper.java
@@ -0,0 +1,20 @@
+package com.example.driving_system_back.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.driving_system_back.entity.CoachStudentList;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+* @author 10162
+* @description 针对表【coach_student_list】的数据库操作Mapper
+* @createDate 2023-06-25 15:28:39
+* @Entity generator.entity.CoachStudentList
+*/
+@Mapper
+public interface CoachStudentListMapper extends BaseMapper<CoachStudentList> {
+
+}
+
+
+
+
